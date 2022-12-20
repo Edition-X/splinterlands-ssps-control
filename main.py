77c73ac44bee64dee29410a229425ed4bae9a108
Dan Kelly <daniel.kelly1@ocado.com>
2022-12-20 22:22:14 +0100

    Update into a Flask app

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+from flask import Flask, render_template, request
 from setup_hive import HIVE_USERNAME, hive
 from setup_logger import logger
 import requests
 import json
+
+app = Flask(__name__)
 
 def get_staked_sps():
     endpoint: str = "https://api.splinterlands.io/players/balances?username="
@@ -18,13 +21,20 @@ def cancel_unstaking():
     hive.custom_json('sm_cancel_unstake_tokens', json_data=json_data, required_auths=[HIVE_USERNAME])
     logger.info("SPS unstaking has been stopped")
 
-
 def unstake(sps):
     json_data: str = '{"token": "SPS", "qty": "%s"}' % (sps)
     hive.custom_json("sm_unstake_tokens", json_data=json_data, required_auths=[HIVE_USERNAME])
     logger.info("SPS unstaking has been started")
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        sps = str(get_staked_sps())
+        cancel_unstaking()
+        unstake(sps)
+        return "Unstaking process started!"
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    sps = str(get_staked_sps())
-    cancel_unstaking()
-    unstake(sps)
+    app.run()
+
